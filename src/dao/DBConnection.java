6dd1ac2dd94086f78cb494a9e18ab5d5488c3d26
andrewjcm <andrew@metzg.us>
2021-10-26 20:16:10 +0000

    Added Dao package and started the DBConnection java file. No other updates.

---
+++ src/dao/DBConnection.java
@@ -0,0 +1,15 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String protocol = "jdbc";
+    private static final String vendorName = "mysql";
+    private static final String ipAddress = "//wgudb.ucertify.com:3306/";
+    private static final String dbName = "client_schedule";
+
+
+}
