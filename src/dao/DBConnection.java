7480b3c51d02c13d469e2f2007f58559c7fde5e3
andrewjcm <andrew@metzg.us>
2021-10-30 04:13:23 +0000

    Fully functional Add/Modify/Delete Appointments.

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -3,7 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import Password.Password;
+import utils.auth.Password;
 
 
 public class DBConnection {
